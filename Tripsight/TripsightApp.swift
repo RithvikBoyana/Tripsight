725efcdcc3920f4d0dd568eb05ddfa43c93cf7ee
RithvikBoyana <rithvikboyana005@tamu.edu>
2025-04-09 00:29:26 -0500

    Initial Commit

---
+++ Tripsight/TripsightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripsightApp.swift
+//  Tripsight
+//
+//  Created by Rithvik Boyana on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+struct TripsightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TripsightTests/TripsightTests.swift
@@ -0,0 +1,17 @@
+//
+//  TripsightTests.swift
+//  TripsightTests
+//
+//  Created by Rithvik Boyana on 4/9/25.
+//
+
+import Testing
+@testable import Tripsight
+
+struct TripsightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
